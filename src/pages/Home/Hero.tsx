4e076b1a80db8a75b0beb5361b2129ab8036b720
Abrar Saleheen <54772944+saleheen19101331@users.noreply.github.com>
2025-04-22 18:05:16 -0500

    feat: update Hero component layout and content for improved user engagement

--- src/pages/Home/Hero.tsx
+++ src/pages/Home/Hero.tsx
@@ -3,7 +3,17 @@ import React from 'react';
 const Hero = () => {
     return (
         <div className= "bg-gradient-to-b from-white to-blue-100 py-20 md:py-32">
-           Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae mollitia laboriosam, voluptate soluta saepe autem architecto pariatur nesciunt eaque deserunt?
+
+           <div className="container mx-auto px-4 text-center flex ">
+           <div className="left" > <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-blue-900 mb-6  ">
+            Track Your Job Applications
+            <span className="text-blue-500 block ">Effortlessly</span>
+            </h1>
+            </div>
+           <div className="right">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum eius amet nam enim facere, harum architecto incidunt ipsa magnam quidem?</p>
+           </div>
+            </div>
         </div>
     );
 };
