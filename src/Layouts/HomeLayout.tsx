d3b1d68b4cc9637f6c01226c737b8656b4a58fe5
Abrar Saleheen <54772944+saleheen19101331@users.noreply.github.com>
2025-04-28 22:08:33 -0500

    Add JobSrCommander component for job search dashboard overview

--- src/Layouts/HomeLayout.tsx
+++ src/Layouts/HomeLayout.tsx
@@ -3,12 +3,14 @@ import Navbar from '../pages/Common/Navbar';
 import Footer from '../pages/Common/Footer';
 import Hero from '../pages/Home/Hero'
 import StreamLine from '../pages/Home/StreamLine';
+import JobSrCommander from '../pages/Home/JobSrCommander';
 const HomeLayout = () => {
   return (
     <div>
       <Navbar></Navbar>
       <Hero></Hero>
       <StreamLine></StreamLine>
+      <JobSrCommander></JobSrCommander>
       <Outlet></Outlet>
       <Footer></Footer>
     </div>
