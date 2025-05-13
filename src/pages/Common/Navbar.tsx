8ca1ea4d24b20f1f52e466e6631e7697f8580421
Abrar Saleheen <54772944+saleheen19101331@users.noreply.github.com>
2025-05-13 13:21:34 -0500

    fix: update links in Navbar and Hero components to use correct routing

--- src/pages/Common/Navbar.tsx
+++ src/pages/Common/Navbar.tsx
@@ -66,9 +66,9 @@ const Navbar = () => {
               );
             } else if (menu === 'About Us') {
               return (
-                <a className="text-xl" key={idx} href="#about">
-                  {menu}
-                </a>
+                  <a className="text-xl" key={idx} href="/about">
+                    {menu}
+                  </a>
               );
             } else {
               return null;
@@ -156,7 +156,7 @@ const Navbar = () => {
                 );
               } else if (menu === 'About Us') {
                 return (
-                  <a className="text-lg py-2 block" key={idx} href="#about">
+                  <a className="text-lg py-2 block" key={idx} href="/about">
                     {menu}
                   </a>
                 );
